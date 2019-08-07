5b0a26ff2320419c406fc511cc3c7872da3e056b
Bryan Szendel <bryan.szendel@gmail.com>
2019-08-07 17:00:33 -0600

    MVP COMPLETE - stretch, styling complete

--- app/src/components/UserLogin.js
+++ app/src/components/UserLogin.js
@@ -13,38 +13,44 @@ const UserLogin = ({ errors, touched, values, status }) => {
   }, [status])
   
   return (
-    <div className="form-container">
-      <h1>My Form</h1>
-      <Form className="input-form">
-        <Field className="input-field" type="text" name="name" placeholder="Name" />
-        {touched.name && errors.name && (
-          <p className="error">{errors.name}</p>
-        )}
-
-        <Field className="input-field" type="text" name="email" placeholder="Email" />
-        {touched.email && errors.email && (
-          <p className="error">{errors.email}</p>
-        )}
-
-        <Field className="input-field" type="text" name="password" placeholder="Password" />
-        {touched.password && errors.password && (
-          <p className="error">{errors.password}</p>
-        )}
-
-        <label className="input-field" id="checkbox"> 
-          <Field type="checkbox" name="tos" />
-          I agree to the terms of service.
-        </label>
-
-        <button className="input-field" type="submit">Submit</button>
-      </Form>
-    <div>
-      {users.map((user) => {
-        return (
-          <p key={user.name} >{user.name}, {user.email}, {user.tos}</p>
-        )
-      })}
-    </div>
+    <div className="container">
+      <div className="form-container">
+        <h1>My Form</h1>
+        <Form className="input-form">
+          <Field className="input-field" type="text" name="name" placeholder="Name" />
+          {touched.name && errors.name && (
+            <p className="error">{errors.name}</p>
+          )}
+  
+          <Field className="input-field" type="text" name="email" placeholder="Email" />
+          {touched.email && errors.email && (
+            <p className="error">{errors.email}</p>
+          )}
+  
+          <Field className="input-field" type="text" name="password" placeholder="Password" />
+          {touched.password && errors.password && (
+            <p className="error">{errors.password}</p>
+          )}
+  
+          <label className="input-field" id="checkbox"> 
+            <Field type="checkbox" name="tos" checked={values.tos}/>
+            I agree to terms of service.
+          </label>
+  
+          <button className="submit-btn" type="submit">Submit</button>
+        </Form>
+      </div>
+      <div className="userlist">
+        {users.map((user) => {
+          return (
+            <div key={user.id}>
+              <p>{user.name}</p>
+              <p>{user.email}</p>
+              <p>{user.tos}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
